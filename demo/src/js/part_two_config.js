5a0ed3bfc4f155c6af4f51423c95f618f5e8822f
linrun <411690557@qq.com>
2017-09-26 00:19:49 +0800

    part_two

--- demo/src/js/part_two_config.js
+++ demo/src/js/part_two_config.js
@@ -3,7 +3,7 @@ export const DEVICE_ONLINE_TIME_CONFIG = {
     title: {
         text: '设备在线时长统计',
         left: "center",
-        top:"1%",
+        top: "1%",
         textStyle: {
             color: "#fff",
             fontSize: 20
@@ -83,11 +83,11 @@ export const DEVICE_ONLINE_TIME_CONFIG = {
     }]
 };
 
-export const DEVICE_HEALTH_TREND_CONFIG = {   
+export const DEVICE_HEALTH_TREND_CONFIG = {
     title: {
         text: '设备健康状态趋势',
         left: "center",
-        top:"1%",
+        top: "1%",
         textStyle: {
             color: "#fff",
             fontSize: 20
@@ -100,13 +100,13 @@ export const DEVICE_HEALTH_TREND_CONFIG = {
         bottom: "18%",
     },
     xAxis: {
-        data: ["一月","二月","三月","四月","五月","六月","七月","八月","九月","十月",],
+        data: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", ],
         offset: 10,
         axisLabel: {
             textStyle: {
                 color: '#fff',
             },
-        }, 
+        },
         axisLine: {
             lineStyle: {
                 color: "#0e6885"
@@ -133,233 +133,135 @@ export const DEVICE_HEALTH_TREND_CONFIG = {
     },
     series: [{
         type: 'bar',
-        barWidth:'25',
-        data:[7000, 1200, 5000, 3000, 2000, 6000, 5600,3333,5012,9000],
-        itemStyle:{
-            normal:{
-                color:'#356da9',
-                borderWidth:2,
-                borderColor:"#072f5b"
+        barWidth: '25',
+        data: [7000, 1200, 5000, 3000, 2000, 6000, 5600, 3333, 5012, 9000],
+        itemStyle: {
+            normal: {
+                color: '#356da9',
+                borderWidth: 2,
+                borderColor: "#072f5b"
             }
         }
-    },
-   ]
+    }, ]
 };
 
 /*激活比例配置*/
-export const ALARM_COUNT_CONFIG = {
- title: {
-     text: '设备告警统计',
-     left: "center",
-     textStyle: {
-         color: "#fff",
-         fontSize: 20
-     }
- },
- color:["#3a7985","#ac484b","#528e69"],
- legend: {
-     orient: "vertical",
-     left: "0%",
-     top: "0%",
-     data: [{
-             name: "通知",
-             textStyle: {
-                 color: "#fff"
-             },
-         },
-         {
-             name: "轻微",
-             textStyle: {
-                 color: "#fff"
-            }
-        },
-        {
-             name: "严重",
-             textStyle: {
-                color: "#fff"
-            }
-        }
-     ]
- },
- series: [
-        {
-            name:'通知',
-            type:'pie',
-            selectedMode: 'single',
-            radius: [0, '36%'],
-            center:["20%","50%"],
-            label: {
-               normal: {
-                   show: true,
-                   lineHeight: 20,
-                   position: 'center',
-                   textStyle: {
-                       fontSize: '18',
-                       color:"#fff",
-                       align:"center"
-                   }
-               },
-              
-           },
-            labelLine: {
-                normal: {
-                    show: false
-                }
-            },
-            data:[
-                 {
-                    value:14,
-                    itemStyle:{
-                       normal:{
-                           color:"#3a7985"      
-                       }
-                   },
-                },
-                {
-                    value:100-14,                    
-                    itemStyle:{
-                       normal:{
-                           color:"transparent"      
-                       }
-                   },
-                },              
-            ]
-        },
-        {
-            type:'pie',
-            name:1231111112,
-            radius: ['40%', '42%'],
-            center:["20%","50%"],
-            label:{
-                normal:{
-                    formatter:"{d}",
-                    color:"#fff",
-                    position:"outside",
-                    align:"center",                                     
-                }
-            },
-            labelLine: {
-                normal: {
-                    show: false
-                }
-            },
-            itemStyle:{
-               normal:{
-                   color:"#2c4877"      
-               }
-           },
-            data:[
-                {value:100},
-              
-            ]
-        },
-         {
-            name:'轻微',
-            type:'pie',
-            selectedMode: 'single',
-            radius: [0, '36%'],
-            center:["50%","50%"],
-            label: {
-                normal: {
-                   show:false
-                }
-            },
-            labelLine: {
-                normal: {
-                    show: false
-                }
-            },
-            data:[
-                 {
-                    value:26,
-                    itemStyle:{
-                       normal:{
-                           color:"#ac484b"      
-                       }
-                   },
-                },
-                {
-                    value:100-26,                    
-                    itemStyle:{
-                       normal:{
-                           color:"transparent"      
-                       }
-                   },
-                },              
-            ]
-        },
-        {
-            type:'pie',
-            radius: ['40%', '42%'],
-            center:["50%","50%"],
-            labelLine: {
-                normal: {
-                    show: false
-                }
-            },
-            itemStyle:{
-               normal:{
-                   color:"#2c4877"      
-               }
-           },
-            data:[
-                {value:100},
-              
-            ]
-        },
-         {
-            name:'严重',
-            type:'pie',
-            selectedMode: 'single',
-            radius: [0, '36%'],
-            center:["80%","50%"],
-            label: {
-                normal: {
-                   show:false
-                }
-            },
-            labelLine: {
-                normal: {
-                    show: false
-                }
-            },
-            data:[
-                 {
-                    value:72,
-                    itemStyle:{
-                       normal:{
-                           color:"#528e69"      
-                       }
-                   },
-                },
-                {
-                    value:100-72,                    
-                    itemStyle:{
-                       normal:{
-                           color:"transparent"      
-                       }
-                   },
-                },              
-            ]
-        },
-        {
-            type:'pie',
-            radius: ['40%', '42%'],
-            center:["80%","50%"],
-            labelLine: {
-                normal: {
-                    show: false
-                }
-            },
-            itemStyle:{
-               normal:{
-                   color:"#2c4877"      
-               }
-           },
-            data:[
-                {value:100},             
-            ]
-        },
+export var ALARM_COUNT_CONFIG = function(chart, data) {
+    const colors = ["#3a7985", "#ac484b", "#528e69"];
+    let series = [];
+    for (let i = 0; i < data.length; i++) {
 
-    ]
+        series.push({
+            name: data[i].name,
+            type: 'pie',
+            selectedMode: 'single',
+            radius: [0, '38%'],
+            center: [(30 * (i + 1) - 10) + "%", "55%"],
+            labelLine: {
+                normal: {
+                    show: false
+                }
+            },
+            markPoint: {
+                symbolOffset: ["-30%", 0],
+                data: [{
+                    symbolSize: 15,
+                    symbolRotate: 0,
+                    itemStyle: {
+                        normal: {
+                            color: 'transparent'
+                        }
+                    },
+                    x: (30 * (i + 1) - 10) + "%",
+                    y: "90%",
+                    label: {
+                        normal: {
+                            position: 'center',
+                            formatter: function() {
+                                return data[i].rate + "%"
+                            },
+                            position: "center",
+                            textStyle: {
+                                color: "#fff",
+                                fontSize:15
+                            }
+                        }
+                    },
+                }]
+            },
+            data: [{
+                    value: data[i].rate,
+                    itemStyle: {
+                        normal: {
+                            color: colors[i]
+                        }
+                    },
+                },
+                {
+                    value: 100 - data[i].rate,
+                    itemStyle: {
+                        normal: {
+                            color: "transparent"
+                        }
+                    },
+                }
+            ]
+        });
+        series.push({
+            type: 'pie',
+            radius: ['42%', '44%'],
+            center: [(30 * (i + 1) - 10) + "%", "55%"],
+            labelLine: {
+                normal: {
+                    show: false
+                }
+            },
+            itemStyle: {
+                normal: {
+                    color: "#2c4877"
+                }
+            },
+            data: [
+                { value: 100 },
+            ]
+        })
+    }
+
+    return {
+        title: {
+            text: '设备告警统计',
+            left: "center",
+            textStyle: {
+                color: "#fff",
+                fontSize: 20
+            }
+        },
+        color: colors,
+        legend: {
+            orient: "vertical",
+            left: "0%",
+            top: "0%",
+            data: [{
+                    name: "通知",
+                    textStyle: {
+                        color: "#fff"
+                    },
+                },
+                {
+                    name: "轻微",
+                    textStyle: {
+                        color: "#fff"
+                    }
+                },
+                {
+                    name: "严重",
+                    textStyle: {
+                        color: "#fff"
+                    }
+                }
+            ]
+        },
+        series
+    }
+
 };
